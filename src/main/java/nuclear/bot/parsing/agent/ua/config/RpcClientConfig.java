45d6dc7ef36db99ad4e0be6dee57aea9819c26ba
VOsipenkov <jumpViaOcean@gmail.com>
2022-11-28 12:38:12 +0300

    Added url for client

--- src/main/java/nuclear/bot/parsing/agent/ua/config/RpcClientConfig.java
+++ src/main/java/nuclear/bot/parsing/agent/ua/config/RpcClientConfig.java
@@ -1,14 +1,21 @@
 package nuclear.bot.parsing.agent.ua.config;
 
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Configuration
 public class RpcClientConfig {
 
-//    @Bean
-//    public JsonRpcHttpClient jsonRpcHttpClient(String url){
-//var temp = new JsonRpcHttpClient();
-//    }
+    @Value("parsingprocessor.url")
+    private String url;
+
+    @Bean
+    public JsonRpcHttpClient jsonRpcHttpClient() throws MalformedURLException {
+        return new JsonRpcHttpClient(new URL(url));
+    }
 }
--- src/main/java/nuclear/bot/parsing/agent/ua/service/MessageRpcServiceClientImpl.java
+++ src/main/java/nuclear/bot/parsing/agent/ua/service/MessageRpcServiceClientImpl.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nuclear.bot.parsingprocessor.dto.AgentMessage;
 import nuclear.bot.parsingprocessor.service.MessageRpcService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
