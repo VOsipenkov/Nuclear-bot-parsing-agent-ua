8229775bfd42736a3b842bcf823b5dbbf3b70806
VOsipenkov <jumpViaOcean@gmail.com>
2022-11-28 13:00:52 +0300

    Added cron-job

--- src/main/java/nuclear/bot/parsing/agent/ua/service/SiteParserService.java
+++ src/main/java/nuclear/bot/parsing/agent/ua/service/SiteParserService.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nuclear.bot.parsingprocessor.dto.AgentMessage;
 import nuclear.bot.parsingprocessor.service.MessageRpcService;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -12,7 +15,16 @@ import org.springframework.stereotype.Service;
 public class SiteParserService {
     private final MessageRpcService messageRpcService;
 
-    public void parseSite() throws Throwable {
-        messageRpcService.sendMessage(new AgentMessage());
+    @Scheduled(fixedRate = 1000)
+    public void executeJob() throws Throwable {
+        messageRpcService.sendMessage(parseSite());
+    }
+
+    public AgentMessage parseSite() {
+        var agentMessage = new AgentMessage();
+        agentMessage.setMessage("test-message");
+        agentMessage.setMessageDateTime(LocalDateTime.now().toString());
+        agentMessage.setParsingAgentName("UA-agent");
+        return agentMessage;
     }
 }
